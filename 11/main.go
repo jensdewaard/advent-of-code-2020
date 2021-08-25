ebae67dac1554bb28d680f1855f4da6f7d121315
Jens de Waard <jcdewaard@pm.me>
2021-08-25 15:10:03 +0000

    Solve day 11 part 2

--- 11/main.go
+++ 11/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "io/ioutil"
-    "log"
-    "strings"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 func readInput(file string) []string {
@@ -16,105 +16,189 @@ func readInput(file string) []string {
 }
 
 func main() {
-    world := readInput("input")
-    
-    changed := true
-    for changed {
-        world, changed = round(world)
-    }
-    
-    for _, l := range world {
-        log.Println(l)
-    }
-    log.Printf("%d seats occupied\n", countSeats(world))
+	world := readInput("input")
+
+	// part one
+	changed := true
+	for changed {
+		world, changed = round(world, ruleOne)
+	}
+	log.Printf("1: %d seats occupied\n", countSeats(world))
+
+	// part two
+	world = readInput("input")
+
+	changed = true
+	for changed {
+		world, changed = round(world, ruleTwo)
+	}
+	log.Printf("2: %d seats occupied\n", countSeats(world))
+
 }
 
 func countSeats(world []string) int {
-    count := 0
-    for _, l := range world {
-        count += strings.Count(l, "#")
-    }
-    return count
+	count := 0
+	for _, l := range world {
+		count += strings.Count(l, "#")
+	}
+	return count
 }
 
 const (
-    OpenSeat = 'L'
-    OccupiedSeat = '#'
-    Floor = '.'
+	OpenSeat     = 'L'
+	OccupiedSeat = '#'
+	Floor        = '.'
+	OutOfBounds  = 'x'
 )
 
-func round(world []string) (newWorld []string, changed bool) {
-    changed = false
-    newWorld = make([]string, len(world))
-    for i := 0; i < len(world); i++ {
-        newWorld[i] = strings.Repeat(".", len(world[0]))
-    }
-    for h := 0; h < len(world); h++ {
-        row := ""
-        for w := 0; w < len(world[0]); w++ {
-            
-            switch getSeatType(world, w, h) {
-            case 'L':
-                if adjacentSeatsOccupied(world, w, h) == 0 {
-                    row += "#"
-                    changed = true
-                } else {
-                    row += "L"
-                }
-            case '#':
-                if adjacentSeatsOccupied(world, w, h) >= 4 {
-                    row += "L"
-                    changed = true
-                } else {
-                    row += "#"
-                }
-            case '.':
-                row += "."
-            }
-        }
-        newWorld[h] = row
-    }
-    
-    return
+type seatingRule func([]string, int, int) (rune, bool)
+
+func ruleOne(world []string, w, h int) (rune, bool) {
+	switch getSeatType(world, w, h) {
+	case OpenSeat:
+		if adjacentSeatsOccupied(world, w, h) == 0 {
+			return OccupiedSeat, true
+		} else {
+			return OpenSeat, false
+		}
+	case OccupiedSeat:
+		if adjacentSeatsOccupied(world, w, h) >= 4 {
+			return OpenSeat, true
+		} else {
+			return OccupiedSeat, false
+		}
+	default:
+		return Floor, false
+	}
+}
+
+func ruleTwo(world []string, w, h int) (rune, bool) {
+	switch getSeatType(world, w, h) {
+	case OpenSeat:
+		if adjacentSeatsOccupiedTwo(world, w, h) == 0 {
+			return OccupiedSeat, true
+		} else {
+			return OpenSeat, false
+		}
+	case OccupiedSeat:
+		if adjacentSeatsOccupiedTwo(world, w, h) >= 5 {
+			return OpenSeat, true
+		} else {
+			return OccupiedSeat, false
+		}
+	default:
+		return Floor, false
+	}
+}
+
+func round(world []string, next seatingRule) (newWorld []string, changed bool) {
+	changed = false
+	newWorld = make([]string, len(world))
+	for i := 0; i < len(world); i++ {
+		newWorld[i] = strings.Repeat(".", len(world[0]))
+	}
+	for h := 0; h < len(world); h++ {
+		row := ""
+		for w := 0; w < len(world[0]); w++ {
+			nextRune, change := next(world, w, h)
+			row += string(nextRune)
+			changed = changed || change
+		}
+		newWorld[h] = row
+	}
+	return
 }
 
 func getSeatType(world []string, x int, y int) rune {
-    if y < 0 || y >= len(world) {
-        return Floor
-    }
-    line := world[y]
-    if x < 0 || x >= len(line) {
-        return Floor
-    }
-    return rune(line[x])
+	if y < 0 || y >= len(world) {
+		return OutOfBounds
+	}
+	line := world[y]
+	if x < 0 || x >= len(line) {
+		return OutOfBounds
+	}
+	return rune(line[x])
 }
 
 func adjacentSeatsOccupied(world []string, x int, y int) int {
-    // top row
-    occupied := 0
-    if getSeatType(world, x - 1, y - 1) == OccupiedSeat {
-        occupied++
-    }    
-    if getSeatType(world, x, y - 1) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x + 1, y - 1) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x - 1, y) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x + 1, y) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x - 1, y + 1) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x, y + 1) == OccupiedSeat {
-        occupied++
-    }
-    if getSeatType(world, x + 1, y + 1) == OccupiedSeat {
-        occupied++
-    }
-    return occupied
+	// top row
+	occupied := 0
+	if getSeatType(world, x-1, y-1) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x, y-1) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x+1, y-1) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x-1, y) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x+1, y) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x-1, y+1) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x, y+1) == OccupiedSeat {
+		occupied++
+	}
+	if getSeatType(world, x+1, y+1) == OccupiedSeat {
+		occupied++
+	}
+	return occupied
 }
+
+func adjacentSeatsOccupiedDir(world []string, x, y int, d Dir) int {
+	switch getSeatType(world, x, y) {
+	case OpenSeat, OutOfBounds:
+		return 0
+	case OccupiedSeat:
+		return 1
+	default:
+		switch d {
+		case UPLEFT:
+			return adjacentSeatsOccupiedDir(world, x-1, y-1, UPLEFT)
+		case UP:
+			return adjacentSeatsOccupiedDir(world, x, y-1, UP)
+		case UPRIGHT:
+			return adjacentSeatsOccupiedDir(world, x+1, y-1, UPRIGHT)
+		case LEFT:
+			return adjacentSeatsOccupiedDir(world, x-1, y, LEFT)
+		case RIGHT:
+			return adjacentSeatsOccupiedDir(world, x+1, y, RIGHT)
+		case DOWNLEFT:
+			return adjacentSeatsOccupiedDir(world, x-1, y+1, DOWNLEFT)
+		case DOWN:
+			return adjacentSeatsOccupiedDir(world, x, y+1, DOWN)
+		case DOWNRIGHT:
+			return adjacentSeatsOccupiedDir(world, x+1, y+1, DOWNRIGHT)
+		}
+		return 0
+	}
+}
+
+func adjacentSeatsOccupiedTwo(world []string, x, y int) int {
+	return adjacentSeatsOccupiedDir(world, x-1, y-1, UPLEFT) +
+		adjacentSeatsOccupiedDir(world, x, y-1, UP) +
+		adjacentSeatsOccupiedDir(world, x+1, y-1, UPRIGHT) +
+		adjacentSeatsOccupiedDir(world, x-1, y, LEFT) +
+		adjacentSeatsOccupiedDir(world, x+1, y, RIGHT) +
+		adjacentSeatsOccupiedDir(world, x-1, y+1, DOWNLEFT) +
+		adjacentSeatsOccupiedDir(world, x, y+1, DOWN) +
+		adjacentSeatsOccupiedDir(world, x+1, y+1, DOWNRIGHT)
+}
+
+type Dir int
+
+const (
+	UPLEFT    Dir = 1
+	UP        Dir = 2
+	UPRIGHT   Dir = 3
+	LEFT      Dir = 4
+	RIGHT     Dir = 5
+	DOWNLEFT  Dir = 6
+	DOWN      Dir = 7
+	DOWNRIGHT Dir = 8
+)
--- 11/main_test.go
+++ 11/main_test.go
@@ -15,12 +15,12 @@ func Test_getSeatType(t *testing.T) {
 		args args
 		want rune
 	}{
-		{"out of bounds x >", args{[]string{"...", "...", "..."}, 5, 2}, '.'},
-		{"out of bounds x <", args{[]string{"...", "...", "..."}, -1, 2}, '.'},
-		{"out of bounds y >", args{[]string{"...", "...", "..."}, 2, 9}, '.'},
-		{"out of bounds y <", args{[]string{"...", "...", "..."}, 1, -3}, '.'},
-		{"no lines", args{[]string{}, 1, 3}, '.'},
-		{"empty strings", args{[]string{"", "", ""}, 1, 3}, '.'},
+		{"out of bounds x >", args{[]string{"...", "...", "..."}, 5, 2}, 'x'},
+		{"out of bounds x <", args{[]string{"...", "...", "..."}, -1, 2}, 'x'},
+		{"out of bounds y >", args{[]string{"...", "...", "..."}, 2, 9}, 'x'},
+		{"out of bounds y <", args{[]string{"...", "...", "..."}, 1, -3}, 'x'},
+		{"no lines", args{[]string{}, 1, 3}, 'x'},
+		{"empty strings", args{[]string{"", "", ""}, 1, 3}, 'x'},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -54,7 +54,6 @@ func Test_adjacentSeatsOccupied(t *testing.T) {
 		{"2,2", args{world, 2, 2}, 6},
 		{"3,3", args{world, 3, 3}, 2},
 		{"2,3", args{world, 3, 2}, 4},
-		
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -63,4 +62,171 @@ func Test_adjacentSeatsOccupied(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_adjacentSeatsOccupiedTwo(t *testing.T) {
+	type args struct {
+		world []string
+		x     int
+		y     int
+	}
+	worldOne := []string{
+		".......#.",
+		"...#.....",
+		".#.......",
+		".........",
+		"..#L....#",
+		"....#....",
+		".........",
+		"#........",
+		"...#.....",
+	}
+	worldTwo := []string{
+		".............",
+		".L.L.#.#.#.#.",
+		".............",
+	}
+	worldThree := []string{
+		".##.##.",
+		"#.#.#.#",
+		"##...##",
+		"...L...",
+		"##...##",
+		"#.#.#.#",
+		".##.##.",
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"example1", args{worldOne, 3, 4}, 8},
+		{"example2a", args{worldTwo, 1, 1}, 0},
+		{"example2b", args{worldTwo, 3, 1}, 1},
+		{"example3", args{worldThree, 3, 3}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := adjacentSeatsOccupiedTwo(tt.args.world, tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("adjacentSeatsOccupied() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_roundRuleOne(t *testing.T) {
+	tests := []struct {
+		name        string
+		world       []string
+		wantWorld   []string
+		wantChanged bool
+	}{
+		{"emptySeatFilled", []string{"...", ".L.", "..."}, []string{"...", ".#.", "..."}, true},
+		{"seatBecomesEmpty", []string{"#.#", ".#.", "###"}, []string{"#.#", ".L.", "###"}, true},
+		{"seatRemainsEmpty", []string{".#.", "L.L", "..."}, []string{".#.", "L.L", "..."}, false},
+		{
+			"example1",
+			[]string{"L.LL.LL.LL", "LLLLLLL.LL", "L.L.L..L..", "LLLL.LL.LL", "L.LL.LL.LL", "L.LLLLL.LL", "..L.L.....", "LLLLLLLLLL", "L.LLLLLL.L", "L.LLLLL.LL"},
+			[]string{"#.##.##.##", "#######.##", "#.#.#..#..", "####.##.##", "#.##.##.##", "#.#####.##", "..#.#.....", "##########", "#.######.#", "#.#####.##"},
+			true,
+		},
+		{
+			"example2",
+			[]string{"#.##.##.##", "#######.##", "#.#.#..#..", "####.##.##", "#.##.##.##", "#.#####.##", "..#.#.....", "##########", "#.######.#", "#.#####.##"},
+			[]string{"#.LL.L#.##", "#LLLLLL.L#", "L.L.L..L..", "#LLL.LL.L#", "#.LL.LL.LL", "#.LLLL#.##", "..L.L.....", "#LLLLLLLL#", "#.LLLLLL.L", "#.#LLLL.##"},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			newWorld, changed := round(tt.world, ruleOne)
+			if !stringSliceEqual(newWorld, tt.wantWorld) || changed != tt.wantChanged {
+				t.Errorf("round() = (%v, %v), want (%v, %v)", newWorld, changed, tt.wantWorld, tt.wantChanged)
+			}
+		})
+	}
+}
+
+func Test_roundRuleTwo(t *testing.T) {
+	tests := []struct {
+		name        string
+		world       []string
+		wantWorld   []string
+		wantChanged bool
+	}{
+		{
+			"example1",
+			[]string{"L.LL.LL.LL", "LLLLLLL.LL", "L.L.L..L..", "LLLL.LL.LL", "L.LL.LL.LL", "L.LLLLL.LL", "..L.L.....", "LLLLLLLLLL", "L.LLLLLL.L", "L.LLLLL.LL"},
+			[]string{"#.##.##.##", "#######.##", "#.#.#..#..", "####.##.##", "#.##.##.##", "#.#####.##", "..#.#.....", "##########", "#.######.#", "#.#####.##"},
+			true,
+		},
+		{
+			"example2",
+			[]string{
+                "#.##.##.##", 
+                "#######.##", 
+                "#.#.#..#..", 
+                "####.##.##", 
+                "#.##.##.##", 
+                "#.#####.##", 
+                "..#.#.....", 
+                "##########", 
+                "#.######.#", 
+                "#.#####.##",
+            },
+			[]string{
+                "#.LL.LL.L#", 
+                "#LLLLLL.LL", 
+                "L.L.L..L..", 
+                "LLLL.LL.LL", 
+                "L.LL.LL.LL", 
+                "L.LLLLL.LL", 
+                "..L.L.....", 
+                "LLLLLLLLL#", 
+                "#.LLLLLL.L", 
+                "#.LLLLL.L#",
+            },
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			newWorld, changed := round(tt.world, ruleTwo)
+			if !stringSliceEqual(newWorld, tt.wantWorld) || changed != tt.wantChanged {
+				t.Errorf("round() = (%v, %v), want (%v, %v)", newWorld, changed, tt.wantWorld, tt.wantChanged)
+			}
+		})
+	}
+}
+
+func Test_countSeats(t *testing.T) {
+	tests := []struct {
+		name  string
+		world []string
+		want  int
+	}{
+		{"5 seats", []string{".L..", "L##L", "##.#"}, 5},
+		{"12 seats", []string{"####", "####", "####"}, 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := countSeats(tt.world)
+			if got != tt.want {
+				t.Errorf("countSeat() = %d, want %d", got, tt.want)
+			}
+		})
+	}
+}
+
+func stringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
